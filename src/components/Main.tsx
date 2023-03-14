0c85db1be20a78e3c210605f8c2330cff2949f0d
Tribhuwan <mathincosmos@gmail.com>
2023-03-14 18:37:20 +0530

    add tailwindcss with cra

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+      <div className="min-h-[100vh] bg-gray-100"></div>
+      <div className="min-h-[100vh] bg-gray-200"></div>
+      <div className="min-h-[100vh] bg-gray-300"></div>
+    </div>
+  );
+}
+
+export default Main
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="h-[100vh] fixed bg-[#bd5d38] w-[18%] min-w-[80px] text-[#868e96]"></div>
+  );
+}
+
+export default Navbar
