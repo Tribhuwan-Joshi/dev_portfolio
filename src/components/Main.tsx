8939a57454b34da1224de162b471c273ae0df573
Tribhuwan <mathincosmos@gmail.com>
2023-03-15 22:27:12 +0530

    Add toggling navbar for small screen

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function Main() {
   return (
-    <div className="flex-1  overflow-auto">
-      <div className="h-[100%] bg-pink-200"></div>
-      <div className="h-[100%] bg-pink-400"></div>
-      <div className="h-[100%] bg-pink-800"></div>
+    <div className="flex-1  overflow-auto scroll-smooth ">
+      <section id="about" className="h-[100%] bg-pink-200"></section>
+      <section id="skills" className="h-[100%] bg-pink-400"></section>
+      <section id="projects" className="h-[100%] bg-pink-800"></section>
+      <section id="interests" className="h-[100%] bg-gray-400"></section>
+      <section id="certifications" className="h-[100%] bg-red-200"></section>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-scroll";
 import pfp from "../assets/pfp.png";
 function Pfp() {
   return (
     <>
       <img
         src={pfp}
-        className="rounded-full max-w-full w-[5rem]"
+        className="rounded-flil max-w-flil w-[5rem]"
         alt="pfp"
         aria-label="pfp of tjsm"
       />
@@ -13,19 +14,94 @@ function Pfp() {
   );
 }
 
-function Navbar() {
+function NavButton({ handleShow }: { handleShow: () => void }) {
   return (
-    <nav className="bg-[#bd5d38] sticky  px-3 tracking-wide py-2 xs:w-[20%] xs:max-w-[17rem] xs:min-w-[8rem] flex justify-between items-center">
-      <h1 className="text-white  xs:hidden text-[1.3rem]  tracking-wider">
-        Tribhuwan Joshi
-      </h1>
-      <button className="border box-content  xs:hidden border-[rgba(255,255,255,.1)] px-4 py-3 rounded-[0.25rem] transition duration-400  ease-in-out  hover:border-white focus:border-white">
-        <div className="hamburger  flex flex-col gap-[0.4rem] justify-evenly">
-          <div className="bar bg-[rgba(255,255,255,.5)] w-[1.4rem] rounded-md   h-[0.10rem] "></div>
-          <div className="bar bg-[rgba(255,255,255,.5)] w-[1.4rem]  rounded-md   h-[0.10rem] "></div>
-          <div className="bar bg-[rgba(255,255,255,.5)] w-[1.4rem]  rounded-md   h-[0.10rem] "></div>
-        </div>
-      </button>
+    <button
+      className="border box-content  xs:hidden border-[rgba(255,255,255,.1)] px-4 py-3 rounded-[0.25rem] transition duration-400  ease-in-out  hover:border-white focus:border-white"
+      onClick={handleShow}
+    >
+      <div className="hamburger  flex flex-col gap-[0.4rem] justify-evenly">
+        <div className="bar bg-[rgba(255,255,255,.5)] w-[1.4rem] rounded-md   h-[0.10rem] "></div>
+        <div className="bar bg-[rgba(255,255,255,.5)] w-[1.4rem]  rounded-md   h-[0.10rem] "></div>
+        <div className="bar bg-[rgba(255,255,255,.5)] w-[1.4rem]  rounded-md   h-[0.10rem] "></div>
+      </div>
+    </button>
+  );
+}
+
+function Navlist({ navShow }: { navShow: boolean }) {
+  return (
+    <div
+      className="bg-[#bd5d38] fixed  text-[rgba(255,255,255,.5)]  w-[100%] left-0 px-4 pb-2 transition-all duration-500 linear"
+      style={{
+        transform: navShow ? "scaleY(100%) " : "scaleY(0)",
+        transformOrigin: "top",
+      }}
+    >
+      <ul className="flex flex-col gap-3 pb-2">
+        <li>
+          <a
+            href="#about"
+            className=" focus:text-white text-[1.2rem] font-medium py-1"
+          >
+            ABOUT
+          </a>
+        </li>
+        <li>
+          <a
+            href="#skills"
+            className=" focus:text-white text-[1.2rem] font-medium py-1"
+          >
+            SKILLS
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#projects"
+            className=" focus:text-white text-[1.2rem] font-medium py-1"
+          >
+            PROJECTS
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#interests"
+            className=" focus:text-white text-[1.2rem] font-medium py-1"
+          >
+            INTERESTS
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#certifications"
+            className=" focus:text-white text-[1.2rem] font-medium py-1"
+          >
+            CERTIFICATIONS
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Navbar() {
+  const [navShow, setNavShow] = useState<boolean>(false);
+  function handleShow(): void {
+    setNavShow((prev) => !prev);
+  }
+  return (
+    <nav className="bg-[#bd5d38] sticky space-y-2  px-4 tracking-wide py-3 xs:w-[20%] xs:max-w-[17rem] xs:min-w-[8rem] ">
+      <div className="navbar flex justify-between items-center">
+        <h1 className="text-white  xs:hidden text-[1.5rem]  tracking-wider">
+          Tribhuwan Joshi
+        </h1>
+        <NavButton handleShow={handleShow} />
+      </div>
+
+      <Navlist navShow={navShow} />
     </nav>
   );
 }
